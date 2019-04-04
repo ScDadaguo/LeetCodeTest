7d97e8007fd2a3b75caf9a8e8516592cb3700c4b
guohao <37768861+ScDadaguo@users.noreply.github.com>
2019-04-04 11:24:33 +0800

    commit

--- src/Solution9.java
+++ src/Solution9.java
@@ -1,2 +1,25 @@
+/** 
+* @Description: 258.各位相加
+* @Param:  
+* @return:  
+* @Author: 文兆杰
+* @Date: 2019/4/4 
+*/ 
 public class Solution9 {
+    public int addDigits(int num) {
+        int sum=0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num = num / 10;
+        }
+        if (sum < 10) {
+            return sum;
+        } else {
+            num=sum;
+            sum=0;
+            addDigits(num);
+            return sum;
+        }
+    }
 }
