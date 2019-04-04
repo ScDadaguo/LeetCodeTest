97dfa05f3a1a58d32c34326fdf1a11c23b56c4b7
guohao <37768861+ScDadaguo@users.noreply.github.com>
2019-04-04 11:40:57 +0800

    commit

--- src/Solution5.java
+++ src/Solution5.java
@@ -1,12 +1,19 @@
 import java.util.List;
-
+/**
+* @Description: 206.反转链表 用递归方式
+* @Param:
+* @return:
+* @Author: 文兆杰
+* @Date: 2019/4/4
+*/
 public class Solution5 {
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode prev = reverseList(head.next);
-
+        ListNode node=reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
         return head;
     }
 
